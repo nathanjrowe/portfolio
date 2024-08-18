ee2fb591dc4ea70fa0b9c9264ca796dc3405e322
Nathan Rowe <naterowe2002@gmail.com>
2024-08-17 21:04:27 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Router, Routes, Route } from 'react-router-dom';
 import { motion } from "framer-motion";
 import './App.css';
 import NavBar from './components/NavBar';
